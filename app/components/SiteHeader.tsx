f9d168ce642fc09539e2b7ac295530065135ffce
manueldev3 <128987217+manueldev3@users.noreply.github.com>
2026-05-18 18:35:55 -0500

    repair responsive

--- app/components/SiteHeader.tsx
+++ app/components/SiteHeader.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { Header } from "antd/es/layout/layout";
 import Link from "next/link";
 import { LoginOutlined } from "@ant-design/icons";
 import { Menu } from "antd";
 
 export default function SiteHeader() {
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useEffect(() => {
+    // Esto solo se ejecuta en el cliente (navegador)
+    setIsDesktop(window.innerWidth > 768);
+  }, []);
+
   return (
     <Header className="justify-between flex items-center p-4">
       <div className="flex items-center space-x-8">
@@ -15,21 +23,16 @@ export default function SiteHeader() {
           theme="dark"
           mode="horizontal"
           defaultSelectedKeys={["1"]}
-          items={[
-            { key: "1", label: "Inicio" },
-            // { key: "2", label: "Naves" },
-            // { key: "3", label: "Comercio" },
-          ]}
+          items={[{ key: "1", label: "Inicio" }]}
         />
       </div>
       <div className="flex items-center space-x-4">
-        {/* <Button type="primary">Download APP</Button> */}
         <Link
           href="https://scg.manueldeveloper.com/app"
           className="flex space-x-2 items-center"
         >
           <LoginOutlined color="#82919E" />
-          {window && window.screen.width > 768 && (
+          {isDesktop && (
             <span className="text-[#82919E] hover:text-[#BCBEC0] no-underline">
               INICIAR SESIÓN
             </span>
